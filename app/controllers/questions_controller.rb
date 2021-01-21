195952afa8cc8ec375fef4a100006132f29f9eb7
roberto silva <roberto.lns10@gmail.com>
2021-01-21 19:55:09 +0000

    rails new

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,17 @@
+class QuestionsController < ApplicationController
+  def ask ; 
+  end
+
+  def answer
+    # raise
+    user_question = params[:user_question]
+
+    if user_question == "I am going to work"
+      @response = "Great"
+    elsif user_question[-1] == "?"
+      @response = "Silly question, get dressed and go to work!"
+    else 
+      @response = "I don't care, get dressed and go to work!"
+    end
+  end
+end
